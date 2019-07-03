051840bfcb7a950e48e3c0a5b38e887f5f1fcd5a
Mohammed Tantawy <mtantawy.mido@gmail.com>
2019-07-03 15:58:23 +0200

    007 - finished signin and handled change signin method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../../components/custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 import './sign-in.styles.scss';
 
 export default class SignIn extends Component {
@@ -14,16 +14,21 @@ export default class SignIn extends Component {
   }
 
   handleChange = event => {
-    const { value, name } = event;
+    const { value, name } = event.target;
     this.setState({
       [name]: value
     });
   };
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
-    // Signin Logic here
-
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.log(error);
+    }
     // Reset form
     this.setState({ email: '', password: '' });
   };
